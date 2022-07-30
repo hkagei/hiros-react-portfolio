97c7ce249bf2be59217cdf420e7b56f0c91cd91a
hkagei <k_dawg_1022@hotmail.com>
2022-07-30 10:45:24 -0600

    update contact section

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './Components/card';
 import Footer from './Components/footer';
-// import Header from './Components/header';
- import Navigation from './Components/navigation';
+import Header from './Components/header';
+//  import Navigation from './Components/navigation';
 // import Project from './Components/project';
 import ContactForm from './Components/contact';
 
 function App() {
   return (
     <div>
-      <Navigation></Navigation>
+      <Header>
+
+      </Header>
       <main>
-      if(!contactSelected){" "}
+      if(!contactSelected){""}
         {
           <>
             {/* <Gallery currentCategory={currentCategory}></Gallery> */}
             <Card></Card>
           </>
-        }{" "}
+        }{""}
         else {<ContactForm></ContactForm>}
         
       </main>
--- src/Components/contact.js
+++ src/Components/contact.js
@@ -1,68 +1,31 @@
-import React, { useState } from "react";
-import {
-    use
-  } from "react-router-dom"; 
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-import { validateEmail } from "../utils/helpers";
+export const ContactForm = () => {
+  const form = useRef();
 
-function ContactForm() {
-  const [formState, setFormState] = useState({ name: "", email: "", message: "", });
-
-  const [errorMessage, setErrorMessage] = useState("");
-  const { name, email, message } = formState;
-  // JSX
-  const handleSubmit = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
-    if(!errorMessage) {
-      setFormState({ [e.target.name]: e.target.value});
-      console.log('Form', formState);
-    }
+
+    emailjs.sendForm('service_wzykamv', 'template_00n08sl', form.current, 'qj0OIOb_YfEd8KKeP')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
   };
 
-  const handleChange = (e) => {
-    if (e.target.name === "email") {
-      const isValid = validateEmail(e.target.value);
-      console.log(isValid);
-      // isValid conditional statement
-      if (!isValid) {
-        setErrorMessage("Your email is invalid.");
-      } else {
-        setErrorMessage('');
-      }
-    } else {
-      if (!e.target.value.length) {
-          setErrorMessage(`${e.target.name} is required.`);
-        } else {
-          setErrorMessage("");
-        }
-      }
-    };
-
   return (
-    <section>
-      <h1 data-testid="h1tag">Contact me</h1>
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="email">Email address:</label>
-          <input type="text" name="email" defaultValue={email} onBlur={handleChange} />
-        </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea type="text" rows="5" defaultValue={message} onBlur={handleChange} />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
-          </div>
-        )}
-        <button data-testid="button" type="submit">Submit</button>
-      </form>
-    </section>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
   );
-}
+};
 
 export default ContactForm;
